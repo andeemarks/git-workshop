fb063d30ebdcd627f3c16ce3c71790d917fb7fa9
Andy Marks <amarks@thoughtworks.com>
2012-05-24 09:37:18 +0800

    Minor rename for consistency

--- test/gmt_offset_finder_spec.rb
+++ test/gmt_offset_finder_spec.rb
@@ -2,37 +2,37 @@ require 'gmt_offset_finder'
 
 describe GMTOffsetFinder do
 	before(:all) do
-		@tf = GMTOffsetFinder.new
+		@finder = GMTOffsetFinder.new
 	end
 
 	it "should find the GMT offset for a known city" do
-		@tf.offsetFor("Melbourne").should_not be_nil
+		@finder.offsetFor("Melbourne").should_not be_nil
 	end
 
 	it "should find the GMT offset for a known city with embedded spaces" do
-		@tf.offsetFor("New York").should_not be_nil
+		@finder.offsetFor("New York").should_not be_nil
 	end
 
 	it "should find the GMT offset for a known city with surrounding spaces" do
-		@tf.offsetFor("Rome  ").should_not be_nil
-		@tf.offsetFor("  Rome  ").should_not be_nil
+		@finder.offsetFor("Rome  ").should_not be_nil
+		@finder.offsetFor("  Rome  ").should_not be_nil
 	end
 
 	it "should treat cities in a case insensitive manner" do
-		@tf.offsetFor("melbourne").should_not be_nil
-		@tf.offsetFor("MELBOURNE").should_not be_nil
-		@tf.offsetFor("MeLBOurNE").should_not be_nil
+		@finder.offsetFor("melbourne").should_not be_nil
+		@finder.offsetFor("MELBOURNE").should_not be_nil
+		@finder.offsetFor("MeLBOurNE").should_not be_nil
 	end
 
 	it "should return numeric offset for a known city" do
-		@tf.offsetFor("Melbourne").should be_a_kind_of Numeric
+		@finder.offsetFor("Melbourne").should be_a_kind_of Numeric
 	end
 
 	it "should return correct offset for a known city" do
-		@tf.offsetFor("Melbourne").should === 10.0
+		@finder.offsetFor("Melbourne").should === 10.0
 	end
 
 	it "should error on an unknown city" do
-		lambda{@tf.offsetFor("Atlantis")}.should raise_error
+		lambda{@finder.offsetFor("Atlantis")}.should raise_error
 	end
 end
