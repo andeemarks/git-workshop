98a061ea5d5012723fd335dff4c44a6f9a6aecef
Andy Marks <amarks@thoughtworks.com>
2012-05-24 12:26:03 +0800

    Added UTC offset for Singapore to cater for environments where timezone is not SGT (e.g., EC2)

--- lib/singapore_relative_clock.rb
+++ lib/singapore_relative_clock.rb
@@ -1,12 +1,14 @@
 require_relative 'gmt_offset_finder'
 
 class SingaporeRelativeClock
+	SGT_OFFSET = "+08:00"
+	
 	def initialize
 		@finder = GMTOffsetFinder.new
 	end
 
 	def timeDifferenceTo(city)
-		singaporeGMTOffset = Time.new.utc_offset / 60 / 60
+		singaporeGMTOffset = Time.new.getlocal(SGT_OFFSET).utc_offset / 60 / 60
 
 		@finder.offsetFor(city) - singaporeGMTOffset
 	end
