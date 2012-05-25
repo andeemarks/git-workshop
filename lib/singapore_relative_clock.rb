d7414654ac1aa84bc5b35ea19e2f745b74880569
Andy Marks <amarks@thoughtworks.com>
2012-05-25 21:19:31 +0800

    Reverted to kickoff state for workshop

--- lib/singapore_relative_clock.rb
+++ lib/singapore_relative_clock.rb
@@ -1,16 +1,8 @@
 require_relative 'gmt_offset_finder'
 
 class SingaporeRelativeClock
-	SGT_OFFSET = "+08:00"
-	
-	def initialize
-		@finder = GMTOffsetFinder.new
-	end
 
 	def timeDifferenceTo(city)
-		singaporeGMTOffset = Time.new.getlocal(SGT_OFFSET).utc_offset / 60 / 60
-
-		@finder.offsetFor(city) - singaporeGMTOffset
 	end
 
 end
--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -5,27 +5,8 @@ describe SingaporeRelativeClock do
 		@clock = SingaporeRelativeClock.new
 	end
 
-	it "should return 0 if the city is in the same timezone as Singapore" do
-		@clock.timeDifferenceTo("Kuala Lumpur").should === 0
+	it "should..." do
+
 	end
 
-	it "should return a negative number if the city is ahead of Singapore's timezone" do
-		@clock.timeDifferenceTo("Sydney").should be > 0
-	end
-
-	it "should return the difference in hours" do
-		@clock.timeDifferenceTo("Adelaide").should be == 1.5 
-	end
-
-	it "should return a positive number if the city is behind of Singapore's timezone" do
-		@clock.timeDifferenceTo("London").should be < 0
-	end
-	
-	it "should return an error if the city is unrecognized" do
-		lambda{@clock.timeDifferenceTo("Koala Lumpy")}.should raise_error
-	end
-	
-	it "should return an error if the city is missing" do
-		lambda{@clock.timeDifferenceTo()}.should raise_error
-	end
 end
